9045d99416e1cc5b0dbd1c9451345e1936fea635
jay <jayp33233@gmail.com>
2023-02-09 17:00:55 +0530

    alternative way to create server and basic routing

--- http server/index.js
+++ http server/index.js
@@ -1,17 +1,33 @@
 const http = require("http");
 const port = 5000;
 
-const server = http.createServer((req,res) =>{
-    res.writeHead(200 , {
+const server = http.createServer();
 
-        'Content-Type' : 'application/json'
-    });
+server.on('request' , (req,res) =>{
+
+    if(req.url === '/friends'){
+        res.statusCode = 200;
+    res.setHeader('Content-Type' , 'application/json')
+
     res.end(JSON.stringify({
         name : "dhruwang",
         role : "fullstack developer",
         age : "20"
     }));
-});
+    }
+    else if(req.url === '/message'){
+        res.setHeader('Content-Type' , 'text/html');
+
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>hi iam jay</h1>')
+        res.write('</body>')
+        res.write('</html>')
+
+        res.end();
+    }
+    
+})
 
 server.listen(port , () =>{
     console.log(`listening on ${port} ...`)
