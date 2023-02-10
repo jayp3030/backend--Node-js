39a243fefac20d9cb08e453cdef9ff1996f91db9
jay <jayp33233@gmail.com>
2023-02-10 14:05:29 +0530

    routing for fetching individual friend

--- express api/server.js
+++ express api/server.js
@@ -2,13 +2,44 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/' , (req ,res)=>{
-    res.send({
-        id : 1,
+const friends = [
+    {
+        id : 0,
         name : "dhiraj" ,
         role : "wordpress developer"
-    })
+    },
+    {
+        id : 1,
+        name : "yash" ,
+        role : "AI/ML engineer"
+    },
+    {
+        id : 2,
+        name : "varun" ,
+        role : "Data Scientist"
+    }
+];
+
+// give all friends info
+app.get('/friends' , (req ,res)=>{
+    res.json(friends);
 });
+
+// for individual friend info using id
+app.get('/friends/:friendId' , (req , res) =>{
+
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+
+    if (friend) {
+        res.status(200).json(friend);
+    }
+    else{
+        res.status(404).json({
+            error : "friend not exist"
+        })
+    }
+})
 
 app.get('/messages' , (req ,res)=>{
     res.send('<h1> hi how are you </h1>')
